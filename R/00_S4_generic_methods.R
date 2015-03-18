3dbc368f0a56c4a0057375860121b71fa96fd2af
Zuguang Gu <jokergoo@gmail.com>
2015-03-18 16:36:24 +0100

    refresh documentations

--- R/00_S4_generic_methods.R
+++ R/00_S4_generic_methods.R
@@ -1,44 +1,44 @@
 
+setGeneric('component_width', function(object, ...) standardGeneric('component_width'))
+
+setGeneric('set_component_height', function(object, ...) standardGeneric('set_component_height'))
+
+setGeneric('draw_annotation_legend', function(object, ...) standardGeneric('draw_annotation_legend'))
+
+setGeneric('draw_heatmap_legend', function(object, ...) standardGeneric('draw_heatmap_legend'))
+
+setGeneric('draw_heatmap_body', function(object, ...) standardGeneric('draw_heatmap_body'))
+
+setGeneric('component_height', function(object, ...) standardGeneric('component_height'))
+
+setGeneric('map_to_colors', function(object, ...) standardGeneric('map_to_colors'))
+
+setGeneric('draw_annotation', function(object, ...) standardGeneric('draw_annotation'))
+
+setGeneric('add_heatmap', function(object, ...) standardGeneric('add_heatmap'))
+
+setGeneric('make_row_cluster', function(object, ...) standardGeneric('make_row_cluster'))
+
+setGeneric('draw_hclust', function(object, ...) standardGeneric('draw_hclust'))
+
+setGeneric('make_layout', function(object, ...) standardGeneric('make_layout'))
+
+setGeneric('color_mapping_legend', function(object, ...) standardGeneric('color_mapping_legend'))
+
+setGeneric('prepare', function(object, ...) standardGeneric('prepare'))
+
 setGeneric('draw', function(object, ...) standardGeneric('draw'))
 
 setGeneric('annotation_legend_size', function(object, ...) standardGeneric('annotation_legend_size'))
 
-setGeneric('make_row_cluster', function(object, ...) standardGeneric('make_row_cluster'))
-
-setGeneric('color_mapping_legend', function(object, ...) standardGeneric('color_mapping_legend'))
-
-setGeneric('component_height', function(object, ...) standardGeneric('component_height'))
-
-setGeneric('make_layout', function(object, ...) standardGeneric('make_layout'))
+setGeneric('draw_heatmap_list', function(object, ...) standardGeneric('draw_heatmap_list'))
 
 setGeneric('get_color_mapping_list', function(object, ...) standardGeneric('get_color_mapping_list'))
 
-setGeneric('set_component_height', function(object, ...) standardGeneric('set_component_height'))
-
-setGeneric('add_heatmap', function(object, ...) standardGeneric('add_heatmap'))
+setGeneric('draw_title', function(object, ...) standardGeneric('draw_title'))
 
 setGeneric('heatmap_legend_size', function(object, ...) standardGeneric('heatmap_legend_size'))
 
-setGeneric('draw_heatmap_list', function(object, ...) standardGeneric('draw_heatmap_list'))
-
-setGeneric('draw_hclust', function(object, ...) standardGeneric('draw_hclust'))
-
-setGeneric('map_to_colors', function(object, ...) standardGeneric('map_to_colors'))
-
-setGeneric('draw_title', function(object, ...) standardGeneric('draw_title'))
-
 setGeneric('make_column_cluster', function(object, ...) standardGeneric('make_column_cluster'))
 
-setGeneric('component_width', function(object, ...) standardGeneric('component_width'))
-
-setGeneric('draw_annotation_legend', function(object, ...) standardGeneric('draw_annotation_legend'))
-
 setGeneric('draw_dimnames', function(object, ...) standardGeneric('draw_dimnames'))
-
-setGeneric('prepare', function(object, ...) standardGeneric('prepare'))
-
-setGeneric('draw_heatmap_body', function(object, ...) standardGeneric('draw_heatmap_body'))
-
-setGeneric('draw_heatmap_legend', function(object, ...) standardGeneric('draw_heatmap_legend'))
-
-setGeneric('draw_annotation', function(object, ...) standardGeneric('draw_annotation'))
