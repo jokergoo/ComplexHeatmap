919badaeeba5002c1537f5dd9a439fac9bc41181
Zuguang Gu <jokergoo@gmail.com>
2017-10-31 13:03:31 +0100

    print startup message

---
+++ R/zzz.R
@@ -0,0 +1,17 @@
+.onAttach = function(libname, pkgname) {
+    version = packageDescription(pkgname, fields = "Version")
+
+  	msg = paste0("========================================
+", pkgname, " version ", version, "
+Bioconductor page: http://bioconductor.org/packages/ComplexHeatmap/
+Github page: https://github.com/jokergoo/ComplexHeatmap
+Documentation: http://bioconductor.org/packages/ComplexHeatmap/
+
+If you use it in published research, please cite:
+Gu, Z. Complex heatmaps reveal patterns and correlations in multidimensional 
+  genomic data. Bioinformatics 2016.
+========================================
+")	
+
+    packageStartupMessage(msg)
+}
