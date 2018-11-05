d84469dc4efb4533c6399a7ddc4558ef516cf9c8
Zuguang Gu <jokergoo@gmail.com>
2018-11-05 11:03:39 +0100

    update

---
+++ tests/testthat/test-multiple-page.R
@@ -0,0 +1,21 @@
+m = matrix(rnorm(100), 10)
+
+postscript("test.ps")
+lgd = Legend(labels = c("a", "b", "c"))
+draw(Heatmap(m), heatmap_legend_list = list(lgd))
+dev.off()
+
+check_pages = function() {
+	lines = readLines("test.ps")
+	print(lines[length(lines)-1])
+	invisible(file.remove("test.ps"))
+}
+
+check_pages()
+
+postscript("test.ps")
+ha = HeatmapAnnotation(foo = 1:10, bar = anno_points(1:10))
+Heatmap(m, top_annotation = ha)
+dev.off()
+
+check_pages()
