fc571fd4783094599652bbfaac2f0e90657da041
Zuguang Gu <jokergoo@gmail.com>
2020-03-08 21:24:17 +0100

    also test svn commit number

--- R/is_abs_unit.R
+++ R/is_abs_unit.R
@@ -85,7 +85,7 @@ is_abs_unit_v4 = function(u) {
 	all(u %in% abs_units)
 }
 
-if(getRversion() >= "4.0.0") {
+if(getRversion() >= "4.0.0" && as.numeric(rv$`svn rev`) >= 77889) {
 	is_abs_unit = is_abs_unit_v4
 } else {
 	is_abs_unit = is_abs_unit_v3
