d5bb3034c1517d8d874558e90f49cbb945a45043
Zuguang Gu <jokergoo@gmail.com>
2020-05-14 13:48:03 +0200

    ht_list is saved into an environment

--- R/selectArea.R
+++ R/selectArea.R
@@ -9,13 +9,14 @@
 #       corresponds to the x and y position of the point.
 # -pos2 Another point as ``pos1``, together with ``pos1`` defines the selected region.
 # -verbose Whether to print messages.
+# -ht_pos A value returned by `ht_pos_on_device`.
 #
 # == details
 # The regions can be selected interactively or manually by setting ``pos1`` and ``pos2``.
 #
 # == value
 # A deep list containing row indices and column indices corresponding to the selected region.
-# The format is (assme the returned object is called ``lt``):
+# The format is (assume the returned object is called ``lt``):
 #
 # - ``lt[[ heatmap_name ]][[ slice_name ]][[ row_index ]]``: row indicies of the selected sub-matrix.
 # - ``lt[[ heatmap_name ]][[ slice_name ]][[ column_index ]]``: column indicies of the selected sub-matrix.
@@ -42,7 +43,8 @@
 # selectArea(ht, pos1 = unit(c(1, 1), "cm"), pos2 = unit(c(8, 8), "cm"))
 # # you can try selectArea(ht) in the interactive graphic device
 #
-selectArea = function(ht_list, pos1 = NULL, pos2 = NULL, mark = TRUE, verbose = TRUE) {
+selectArea = function(ht_list, pos1 = NULL, pos2 = NULL, mark = TRUE, verbose = TRUE,
+	ht_pos = NULL) {
 
 	if(is.null(pos1) || is.null(pos2)) pos2 = pos1 = NULL
 	if(!is.null(pos1)) {
@@ -69,7 +71,7 @@ selectArea = function(ht_list, pos1 = NULL, pos2 = NULL, mark = TRUE, verbose = 
 		pos2 = list(x = pos2[1], y = pos2[2])
 	}
 
-	seekViewport("global")
+	if(mark) seekViewport("global")
 	
 	if(is.null(pos1)) {
 		if(!dev.interactive()) {
@@ -89,7 +91,7 @@ selectArea = function(ht_list, pos1 = NULL, pos2 = NULL, mark = TRUE, verbose = 
 	}
 	pos1 = lapply(pos1, as.numeric)
 	if(mark) grid.points(pos1$x, pos1$y, default.units = unit)
-	if(verbose) qqcat("  Point1: x = @{sprintf('%.1f', pos1$x)} @{unit}, y = @{sprintf('%.1f', pos1$y)} @{unit} (measured in the graphic device)\n")
+	if(verbose) qqcat("  Point 1: x = @{sprintf('%.1f', pos1$x)} @{unit}, y = @{sprintf('%.1f', pos1$y)} @{unit} (measured in the graphic device)\n")
 	
 	if(is.null(pos2)) {
 		unit = "mm"
@@ -103,7 +105,7 @@ selectArea = function(ht_list, pos1 = NULL, pos2 = NULL, mark = TRUE, verbose = 
 	}
 	pos2 = lapply(pos2, as.numeric)
 	if(mark) grid.points(pos2$x, pos2$y, default.units = unit)
-	if(verbose) qqcat("  Point2: x = @{sprintf('%.1f', pos2$x)} @{unit}, y = @{sprintf('%.1f', pos2$y)} @{unit} (measured in the graphic device)\n")
+	if(verbose) qqcat("  Point 2: x = @{sprintf('%.1f', pos2$x)} @{unit}, y = @{sprintf('%.1f', pos2$y)} @{unit} (measured in the graphic device)\n")
 	
 	if(verbose) cat("\n")
 
@@ -125,7 +127,11 @@ selectArea = function(ht_list, pos1 = NULL, pos2 = NULL, mark = TRUE, verbose = 
 		pos2$y = tmp
 	}
 
-	ht_pos = ht_pos_on_device(ht_list, unit, valueOnly = TRUE)
+	if(is.null(ht_pos)) ht_pos = ht_pos_on_device(ht_list)
+	ht_pos = lapply(ht_pos, function(x) lapply(x, function(y) {
+		list(x = convertX(y$x, unit, valueOnly = TRUE),
+		     y = convertY(y$y, unit, valueOnly = TRUE))
+	}))
 
 	res = list()
 	for(ht_name in names(ht_pos)) {
@@ -179,6 +185,134 @@ selectArea = function(ht_list, pos1 = NULL, pos2 = NULL, mark = TRUE, verbose = 
 				} else {
 					res[[ht_name]][[vp_name]]$row_index = ht@row_order_list[[i]][ind2:ind1]
 				}
+			}
+
+			if(length(res[[ht_name]][[vp_name]]$column_index) == 0) res[[ht_name]][[vp_name]]$row_index = integer(0)
+			if(length(res[[ht_name]][[vp_name]]$row_index) == 0) res[[ht_name]][[vp_name]]$column_index = integer(0)
+			
+			if(length(res[[ht_name]][[vp_name]]$column_index) == 0) {
+				if(verbose) cat("not overlap\n")
+			} else {
+				if(verbose) cat("overlap\n")
+			}
+		}
+	}
+	if(verbose) cat("\n")
+	res
+}
+
+# == title
+# Select a position in the heatmap
+#
+# == param
+# -ht_list A `HeatmapList-class` object returned by `draw,Heatmap-method` or `draw,HeatmapList-method`.
+# -mark Whether mark the selected position as a point.
+# -pos If the value is ``NULL``, it can be selected by click on the heatmap (of cource, the heatmap should be on
+#       the interactive graphic device). If it is set, it must be a `grid::unit` object with length two which
+#       corresponds to the x and y position of the point.
+# -verbose Whether to print messages.
+# -ht_pos A value returned by `ht_pos_on_device`.
+#
+# == value
+# A deep list containing row index and column index corresponding to the selected position.
+# The format is (assume the returned object is called ``lt``):
+#
+# - ``lt[[ heatmap_name ]][[ slice_name ]][[ row_index ]]``: row index of the selected point.
+# - ``lt[[ heatmap_name ]][[ slice_name ]][[ column_index ]]``: column index of the selected point.
+#
+# == author
+# Zuguang Gu <z.gu@dkfz.de>
+#
+# == example
+# m = matrix(rnorm(100), 10)
+# rownames(m) = 1:10
+# colnames(m) = 1:10
+#
+# ht = Heatmap(m)
+# ht = draw(ht)
+# selectPosition(ht, pos = unit(c(4, 4), "cm"))
+# # you can try selectArea(ht) in the interactive graphic device
+#
+selectPosition = function(ht_list, pos = NULL, mark = TRUE, verbose = TRUE,
+	ht_pos = NULL) {
+
+	pos1 = pos
+	if(!is.null(pos1)) {
+		if(!inherits(pos1, "simpleUnit")) {
+			stop_wrap("`pos` should be a simpleUnit.")
+		}
+
+		if(length(pos1) != 2) {
+			stop_wrap("Length of `pos` should be 2 (x and y).")
+		}
+
+		pos1 = list(x = pos1[1], y = pos1[2])
+	}
+
+	if(mark) seekViewport("global")
+	
+	if(is.null(pos1)) {
+		if(!dev.interactive()) {
+			stop_wrap("Graphic device should be interactive if you want to manually select the position.")
+		}
+
+		if(verbose) cat("Click one position on the heatmap (right click on the plot to cancel):\n")
+
+		unit = "mm"
+		pos1 = grid.locator(unit = unit)
+		if(is.null(pos1)) {
+			if(verbose) cat("Canceled.\n")
+			return(invisible(NULL))
+		}
+	} else {
+		unit = unitType(pos1$x)
+	}
+	pos1 = lapply(pos1, as.numeric)
+	if(mark) grid.points(pos1$x, pos1$y, default.units = unit)
+	if(verbose) qqcat("  Point: x = @{sprintf('%.1f', pos1$x)} @{unit}, y = @{sprintf('%.1f', pos1$y)} @{unit} (measured in the graphic device)\n")
+	
+	if(verbose) cat("\n")
+
+	if(is.null(ht_pos)) ht_pos = ht_pos_on_device(ht_list)
+	ht_pos = lapply(ht_pos, function(x) lapply(x, function(y) {
+		list(x = convertX(y$x, unit, valueOnly = TRUE),
+		     y = convertY(y$y, unit, valueOnly = TRUE))
+	}))
+
+	res = list()
+	for(ht_name in names(ht_pos)) {
+
+		ht = ht_list@ht_list[[ht_name]]
+		res[[ht_name]] = list()
+
+		if(verbose) qqcat("Search in heatmap '@{ht_name}'\n")
+
+		for(vp_name in names(ht_pos[[ht_name]])) {
+			i = as.numeric(gsub(".*_(\\d+)_\\d+$", "\\1", vp_name))
+			j = as.numeric(gsub(".*_(\\d+)$", "\\1", vp_name))
+
+			if(verbose) qqcat("  - row slice @{i}, column slice @{j} [@{vp_name}]... ")
+			
+			vp_min_x = ht_pos[[ht_name]][[vp_name]]$x[1]	
+			vp_max_x = ht_pos[[ht_name]][[vp_name]]$x[2]
+			vp_min_y = ht_pos[[ht_name]][[vp_name]]$y[1]	
+			vp_max_y = ht_pos[[ht_name]][[vp_name]]$y[2]
+
+			nc = length(ht@column_order_list[[j]])
+			ind1 = ceiling((pos1$x - vp_min_x) / (vp_max_x - vp_min_x) * nc)
+
+			if(ind1 <= 0 || ind1 > nc) { # the region is on the left of the heatmap
+				res[[ht_name]][[vp_name]]$column_index = integer(0)
+			} else {
+				res[[ht_name]][[vp_name]]$column_index = ht@column_order_list[[j]][ind1]
+			}
+
+			nr = length(ht@row_order_list[[i]])
+			ind1 = 1 + nr - ceiling((pos1$y - vp_min_y) / (vp_max_y - vp_min_y) * nr)
+			if(ind1 <= 0  || ind1 > nr) { # the region is on the bottom of the heatmap
+				res[[ht_name]][[vp_name]]$row_index = integer(0)
+			} else {
+				res[[ht_name]][[vp_name]]$row_index = ht@row_order_list[[i]][ind1]
 			}
 
 			if(length(res[[ht_name]][[vp_name]]$column_index) == 0) res[[ht_name]][[vp_name]]$row_index = integer(0)
@@ -339,10 +473,26 @@ selectArea_shiny_demo = function(ht_list) {
 		rownames(m) = 1:10
 		colnames(m) = 1:10
 
-		ht_list = Heatmap(m)
+		ht_list = Heatmap(m) + NULL
 	} else {
 		ht_list = ht_list
 	}
 	source(system.file("app", "app.R", package = "ComplexHeatmap"), local = TRUE)
 	shiny::shinyApp(get("ui"), get("server"))
+}
+
+get_pos_from_brush = function(brush) {
+	coords = brush$coords_css
+    height = (brush$range$bottom - brush$range$top)/brush$img_css_ratio$y
+    pos1 = unit(c(coords$xmin, height - coords$ymin), "pt")
+    pos2 = unit(c(coords$xmax, height - coords$ymax), "pt")
+
+    list(pos1, pos2)
+}
+
+get_pos_from_click = function(click) {
+	coords = click$coords_css
+    height = (click$range$bottom - click$range$top)/click$img_css_ratio$y
+    pos1 = unit(c(coords$x, height - coords$y), "pt")
+    pos1
 }
--- inst/app/app.R
+++ inst/app/app.R
@@ -1,46 +1,66 @@
 require(shiny)
+library(GetoptLong)
 
 ui = fluidPage(
 	fluidRow(
-		column(width = 6,
+		column(width = 3,
 			plotOutput("plot1", height = 300, width = 300,
-		        brush = brushOpts(id = "plot1_brush")
-		     )
+		        brush = brushOpts(id = "plot1_brush"),
+		        click = "plot1_click"
+		    ),
+			verbatimTextOutput("click_info")
 		),
-		column(width = 6,
+		column(width = 3,
 		  plotOutput("brush_info", height = 300, width = 300)
 		)
 	)
 )
 
+shiny_env = new.env()
+
 server = function(input, output) {
 	output$plot1 = renderPlot({
-		draw(ht_list)
+		shiny_env$ht_list = draw(ht_list)
+		shiny_env$ht_pos = ht_pos_on_device(shiny_env$ht_list)
 	})
 
 	output$brush_info = renderPlot({
 		if(is.null(input$plot1_brush)) {
-			
+			grid.newpage()
+			grid.text("No region is selected.", 0.5, 0.5)
 		} else {
-		  	coords = input$plot1_brush$coords_css
-		    rg = input$plot1_brush$range
-		    height = (rg$bottom - rg$top)/input$plot1_brush$img_css_ratio$y
-		    pos1 = unit(c(coords$xmin, height - coords$ymin), "pt")
-		    pos2 = unit(c(coords$xmax, height - coords$ymax), "pt")
-
-		    # png("foo.png", width = rg$right - rg$left, height = rg$bottom - rg$top)
-		    # this makes a plot, but it will be overwritten by the next one
-		    ht_list = draw(ht_list)
-		    pos = selectArea(ht_list, pos1 = pos1, pos2 = pos2, verbose = FALSE)
-		    # dev.off()
-		    # file.remove("foo.png")
+		  	lt = ComplexHeatmap:::get_pos_from_brush(input$plot1_brush)
+		  	pos1 = lt[[1]]
+		  	pos2 = lt[[2]]
+		    
+		    ht_list = shiny_env$ht_list
+		    pos = selectArea(ht_list, mark = FALSE, pos1 = pos1, pos2 = pos2, verbose = FALSE, ht_pos = shiny_env$ht_pos)
+		    
 		    row_index = pos[[1]][[1]]$row_index
 		    column_index = pos[[1]][[1]]$column_index
+		    m = ht_list@ht_list[[1]]@matrix
 		    ht_select = Heatmap(m[row_index, column_index, drop = FALSE],
 		    	col = ht_list@ht_list[[1]]@matrix_color_mapping@col_fun,
 		    	show_heatmap_legend = FALSE,
 		    	cluster_rows = FALSE, cluster_columns = FALSE)
 		    draw(ht_select)
+		}
+	})
+
+	output$click_info = renderText({
+		if(is.null(input$plot1_click)) {
+			"Not selected."
+		} else {
+			pos1 = ComplexHeatmap:::get_pos_from_click(input$plot1_click)
+		    
+		    ht_list = shiny_env$ht_list
+		    pos = selectPosition(ht_list, mark = FALSE, pos = pos1, verbose = FALSE, ht_pos = shiny_env$ht_pos)
+			
+			row_index = pos[[1]][[1]]$row_index
+		    column_index = pos[[1]][[1]]$column_index
+		    m = ht_list@ht_list[[1]]@matrix
+		    v = m[row_index, column_index]
+			qq("row index: @{row_index}\ncolumn index: @{column_index}\nvalue: @{v}")
 		}
 	})
 }
