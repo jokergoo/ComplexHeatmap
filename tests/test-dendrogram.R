c506e841444081f1f1c22752948b5b7503bf8f53
Zuguang Gu <jokergoo@gmail.com>
2020-07-06 23:05:25 +0200

    update docs

--- tests/test-dendrogram.R
+++ tests/test-dendrogram.R
@@ -79,17 +79,17 @@ dend = as.dendrogram(hclust(dist(m)))
 grid.dendrogram(dend, test = T)
 
 # node attr
-m = matrix(rnorm(100), 10)
-dend = as.dendrogram(hclust(dist(m)))
-require(dendextend)
-dend1 = color_branches(dend, k = 2, col = 1:2)
-grid.dendrogram(dend1, test = T)
-dend1 = dend
-dend1 = dendrapply(dend, function(d) {
-	attr(d, "nodePar") = list(pch = 13, size = unit(runif(1, min = 3, max = 20), "bigpts"))
-	d
-})
-grid.dendrogram(dend1, test = T)
+# m = matrix(rnorm(100), 10)
+# dend = as.dendrogram(hclust(dist(m)))
+# require(dendextend)
+# dend1 = color_branches(dend, k = 2, col = 1:2)
+# grid.dendrogram(dend1, test = T)
+# dend1 = dend
+# dend1 = dendrapply(dend, function(d) {
+# 	attr(d, "nodePar") = list(pch = 13, size = unit(runif(1, min = 3, max = 20), "bigpts"))
+# 	d
+# })
+# grid.dendrogram(dend1, test = T)
 
-Heatmap(m, cluster_rows = dend1, cluster_columns = dend1)
+# Heatmap(m, cluster_rows = dend1, cluster_columns = dend1)
 
